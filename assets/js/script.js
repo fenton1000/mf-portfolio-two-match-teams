584f11289ff4ed87906e56c56151a60fb41e3b50
fenton1000 <fenton1000@yahoo.co.uk>
2023-01-07 12:41:17 +0000

    Create basic addPlayer function to add player objects to player array

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,19 @@ document.addEventListener('DOMContentLoaded', function() {
     let submitPlayerName = document.getElementById('submit-player-name');
     submitPlayerName.addEventListener('click', addPlayer);
 });
+
+/**
+ * Function addPlayer adds user inputted player name to player object array.
+ */
+function addPlayer() {
+    let newPlayer = document.getElementById('enter-name').value;
+
+    let playerObject = {
+        playerName: newPlayer,
+        points: 0
+    };
+
+    players.push(playerObject);
+
+    console.log(players);
+}
