3a2470a7276936de6d1e8abfd64f2eff79307b96
fenton1000 <fenton1000@yahoo.co.uk>
2023-01-07 12:47:25 +0000

    Add for loop to addPlayer function to prevent same name being entered more than once

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,14 @@ document.addEventListener('DOMContentLoaded', function() {
 function addPlayer() {
     let newPlayer = document.getElementById('enter-name').value;
 
+    for (player of players) {
+        let nameToCheck = player.playerName;
+        if (newPlayer === nameToCheck) {
+            alert('Player name already exists! Please enter alternative name');
+            throw 'Name already exists!';
+        }
+    }
+
     let playerObject = {
         playerName: newPlayer,
         points: 0
